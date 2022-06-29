54c9d82f0e301dce094398f251c50483c4be2721
Justin-mac <ljustin515@gmail.com>
2022-06-29 12:28:27 -0600

    finished fetch GET session, POST session, user

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 4. Get some inputs and buttons
 
 5. implement fetch requests
+    Login Steps:
+        GET session (200,401)
+        POST session (201, 401)
+        POST user (201, 4--)
+
+    Add Pages for Login/Registration/HomePage if logged in/Errors show if users logging in.
+        Bad login
+        bad new email
+        empty fields
+        
---
+++ app.js
@@ -0,0 +1,112 @@
+const URL = "https://forum2022.codeschool.cloud"
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        loginEmailInput: "",
+        loginPasswordInput: "",
+
+        newNameInput: "",
+        newEmailInput: "",
+        newPasswordInput: "",
+
+    },
+    methods: {
+        // Get /session - Ask the server if we are logged in
+        getSession: async function () {
+            let response = await fetch(`${URL}/session`,{
+                method: "GET",
+                credentials: "include"
+            });
+            //Are we logged in?
+            if (response.status == 200) {
+                //logged in 
+                console.log("logged in");
+                let data = await response.json();
+                console.log(data);
+            } else if (response.status == 401) {
+                //not logged in
+                console.log("not logged in");
+                let data = await response.json();
+                console.log(data);
+            } else {
+                console.log("Some sort of error when GETTING /session:", response.status, response);
+            }
+        },
+        // POST /session - Attempt to login
+        postSession: async function () {
+            let loginCredentials = {
+                username: this.loginEmailInput, 
+                password: this.loginPasswordInput};
+
+            let response = await fetch(URL + "/session", {
+                method: "POST",
+                body: JSON.stringify(loginCredentials),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+            // parse the body
+            let body = response.json();
+            console.log(body);
+            // was the login sucessful
+            if (response.status == 201) {
+                console.log("Successful Login Attempt");
+
+                //clear inputs
+                this.loginEmailInput = "";
+                this.loginPasswordInput = "";
+            } else if (response.status == 401) {
+                console.log("Unsuccessful Login Attempt");
+
+                //let user know it was unsuccessful
+                alert("Unsuccessful login");
+
+
+                
+                //clear password input
+                this.loginPasswordInput = "";
+            } else {
+                console.log("Some sort of error when POSTING /session:", response.status, response);
+            }
+        },
+        postUser: async function () {
+            let userCredentials = {
+                username: this.newEmailInput, 
+                fullname: this.newNameInput,
+                password: this.newPasswordInput};
+            let response = await fetch(URL + "/user", {
+                method: "POST",
+                body: JSON.stringify(userCredentials),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                
+            });
+            // parse the body
+            let body = response.json();
+            console.log(body);
+            // was the new user was created
+
+            //Success
+            if (response.status == 201) {
+                console.log("User Created");
+
+                //clear inputs
+                this.newEmailInput = "";
+                this.newPasswordInput = "";
+                this.newNameInput = "";
+            } else if (response.status == 401) {
+                console.log("Unsuccessful Creation");                
+            } else {
+                console.log("Some sort of error when creating", response.status, response);
+            }
+        }
+        // POST /user - create new user
+    },
+    created: function () {
+        this.getSession();
+    }
+})
